3e3d8f5db434c566484197663379c2153af3299b
Jacob Feld <jfeld@pillartechnology.com>
2018-08-29 12:23:45 -0500

    Added cors support to forgeItem function

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,16 @@
 const functions = require('firebase-functions');
 const Forge = require('./Forge/Forge');
 const forge = new Forge();
+const cors = require('cors')({origin: true});
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.status(200).json({message: 'Hello from Firebase!'});
 });
 
 exports.forgeItem = functions.https.onRequest((req, res) => {
-  let item = forge.forgeItem();
-  let message = {message: 'I made an item!', item: item.jsonify(), readout: item.readOut()};
-  res.status(200).json(message);
+  cors(req, res, () => {
+    let item = forge.forgeItem();
+    let message = {message: 'I made an item!', item: item.jsonify(), readout: item.readOut()};
+    res.status(200).json(message);
+  })
 });
